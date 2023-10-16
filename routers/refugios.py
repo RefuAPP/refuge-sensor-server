0cb605754af103e3e329ca03500bb2a05fa69863
alexm3133 <alexandrumateiionut@gmail.com>
2023-10-16 20:04:49 +0200

    .

--- routers/refugios.py
+++ routers/refugios.py
@@ -55,10 +55,10 @@ async def get_weekly_count_by_day(id_refugio: str, start_date: date = None, end_
 
     try:
         cursor.execute("""
-            SELECT DATE(timestamp), current_count
+            SELECT DATE(timestamp), SUM(people_in) - SUM(people_out)
             FROM eventos
             WHERE id_refugio = %s AND DATE(timestamp) BETWEEN %s AND %s
-            GROUP BY DATE(timestamp), current_count
+            GROUP BY DATE(timestamp)
             ORDER BY DATE(timestamp)
         """, (id_refugio, start_date, end_date))
 
