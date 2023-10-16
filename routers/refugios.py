ce135639b9c7b5f9bd6ccb553c59e06c83a8115f
alexm3133 <alexandrumateiionut@gmail.com>
2023-10-16 19:50:22 +0200

    adding more logic to get_weekly_count

--- routers/refugios.py
+++ routers/refugios.py
@@ -24,18 +24,11 @@ async def get_daily_count(id_refugio: str, day: date = None):
         day = date.today()
 
     try:
-        cursor.execute("""
-            SELECT SUM(people_in) - SUM(people_out)
-            FROM eventos
-            WHERE id_refugio = %s AND DATE(timestamp) = %s
-        """, (id_refugio, day))
-
+        cursor.execute("SELECT current_count FROM refugios WHERE id_refugio = %s", (id_refugio,))
         result = cursor.fetchone()
-
         if result is None:
             raise HTTPException(status_code=404, detail="Refugio no encontrado")
-
-        daily_count = max(result[0], 0) if result[0] is not None else 0
+        daily_count = result[0]
 
         return {"daily_count": daily_count, "day": str(day)}
  
@@ -62,10 +55,10 @@ async def get_weekly_count_by_day(id_refugio: str, start_date: date = None, end_
 
     try:
         cursor.execute("""
-            SELECT DATE(timestamp), SUM(people_in) - SUM(people_out)
+            SELECT DATE(timestamp), current_count
             FROM eventos
             WHERE id_refugio = %s AND DATE(timestamp) BETWEEN %s AND %s
-            GROUP BY DATE(timestamp)
+            GROUP BY DATE(timestamp), current_count
             ORDER BY DATE(timestamp)
         """, (id_refugio, start_date, end_date))
 
@@ -84,42 +77,4 @@ async def get_weekly_count_by_day(id_refugio: str, start_date: date = None, end_
     except Exception as e:
         conn.rollback()
         return JSONResponse(content={"detail": str(e)}, status_code=500)
-    
-@router.get("/refugio/{id_refugio}/interval_count/",
-            response_model=IntervalCountResponse,
-            responses={
-                200: {"description": "Operación exitosa"},
-                401: {"description": "Error: Unauthorized", "model": UnauthorizedResponse},
-                403: {"description": "Error: Forbidden", "model": ForbiddenResponse},
-                404: {"description": "Error: Not Found", "model": NotFoundResponse},
-                422: {"description": "Validation Error", "model": ValidationErrorResponse},
-                500: {"description": "Internal Server Error", "model": InternalServerErrorResponse}
-            },
-            summary="Obtiene el número de entradas y salidas de personas para un refugio específico en un intervalo de fechas",
-            description="Si no se proporcionan fechas de inicio y fin, se usa la semana actual",
-            response_description="Retorna el número de personas que han entrado y salido")
-async def get_interval_count(id_refugio: str, start_date: date = None, end_date: date = None):
-    if start_date is None or end_date is None:
-        end_date = date.today()
-        start_date = end_date - timedelta(days=6)
 
-    try:
-        cursor.execute("""
-            SELECT SUM(people_in), SUM(people_out)
-            FROM eventos
-            WHERE id_refugio = %s AND DATE(timestamp) BETWEEN %s AND %s
-        """, (id_refugio, start_date, end_date))
-
-        result = cursor.fetchone()
-
-        if result is None:
-            raise HTTPException(status_code=404, detail="Refugio no encontrado")
-
-        people_in = result[0] if result[0] is not None else 0
-        people_out = result[1] if result[1] is not None else 0
-
-        return {"people_in": people_in, "people_out": people_out, "start_date": str(start_date), "end_date": str(end_date)}
-        
-    except Exception as e:
-        conn.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
